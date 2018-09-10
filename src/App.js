bf3f91fd945700dccc1dddd03ac747f81ab1d64c
Jay J <mis@thewoodagency.com>
2018-09-10 10:33:10 -0500

    change text on app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React on Docker !! </h1>
         </header>
         <p className="App-intro">
-         Dockerfile vs Dockerfile.dev with docker-compose.yml
+         Changes are made on feature branch...
         </p>
       </div>
     );
